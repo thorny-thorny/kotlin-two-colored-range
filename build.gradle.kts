e9aa6a96b31b078cd7693b19664204ee89f7f715
thorny <35000252+thorny-thorny@users.noreply.github.com>
2022-01-22 16:04:00 +0700

    Attempt to fix ndoejs@arm64

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.5.10"
+    kotlin("multiplatform") version "1.6.10"
     id("org.jetbrains.dokka") version "1.6.10"
     id("maven-publish")
 }
@@ -43,7 +43,6 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
